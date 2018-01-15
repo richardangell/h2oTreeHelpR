b32b69ec4583481bae177731c0d990f24d51c13c
richardangell <richardangell37@gmail.com>
2018-01-15 18:34:33 +0000

    give fcns better names

---
+++ H2OTreeConvertR/R/gv_to_table.R
@@ -0,0 +1,216 @@
+#' Convert h2o tree .gv file to tabular structure
+#'
+#' Reads a single .gv file containing a tree from a h2o gbm or drf and parses it
+#' into a tabular structure
+#'
+#' @param gv_file .gv file file including path to import.
+#'
+#' @return \code{data.frame} containing tree structure.
+#'
+#' @export
+gv_to_table <- function(gv_file) {
+
+  #----------------------------------------------------------------------------#
+  # Function Layout: ----
+  # Section 0. Input checking
+  # Section 1. Import gv file
+  # Section 2. Get nodes in table
+  # Section 3. Get terminal node predictions
+  # Section 4. Get split information
+  # Section 5. Return tree in tabular structure
+  #----------------------------------------------------------------------------#
+
+  #----------------------------------------------------------------------------#
+  # Section 0. Input checking ----
+  #----------------------------------------------------------------------------#
+
+  if (tools::file_ext(gv_file) != "gv") {
+
+    stop("gv_file does not have .gv extension")
+
+  }
+
+  #----------------------------------------------------------------------------#
+  # Section 1. Import gv file ----
+  #----------------------------------------------------------------------------#
+
+  gv_import <- readLines(gv_file)
+
+  n <- length(gv_import)
+
+  #----------------------------------------------------------------------------#
+  # Section 2. Get nodes in table ----
+  #----------------------------------------------------------------------------#
+
+  edges_line <- which(gv_import == "/* Edges */")
+
+  end_edges_line <- which(gv_import[edges_line:n] == "")[1] + edges_line - 1
+
+  nodes_line <- which(gv_import == "/* Nodes */")
+
+  open_brackets_nodes_lines <- which(gv_import[nodes_line:edges_line] == '{') + nodes_line - 1
+
+  close_brackets_nodes_lines <- which(gv_import[nodes_line:edges_line] == '}') + nodes_line - 1
+
+  # extract rows from the imported file that contain node information; between '{' and  '}'
+  node_levels_list <- mapply(function(x, y) gv_import[(x + 1):(y - 1)],
+                             open_brackets_nodes_lines,
+                             close_brackets_nodes_lines,
+                             SIMPLIFY = FALSE)
+
+  node_levels <- unlist(node_levels_list)
+
+  node_levels <- gsub("\"", "", node_levels)
+
+  node_levels_split <- strsplit(node_levels, " \\[")
+
+  node_table <- data.frame(node = sapply(node_levels_split, "[", 1),
+                           node_text = paste0("[", sapply(node_levels_split, "[", 2)),
+                           stringsAsFactors = FALSE)
+
+  #----------------------------------------------------------------------------#
+  # Section 3. Get terminal node predictions ----
+  #----------------------------------------------------------------------------#
+
+  node_text_split <- strsplit(node_table$node_text, "label=")
+
+  label_values <- sapply(node_text_split, "[", 2)
+
+  label_values <- substr(label_values, 1, nchar(label_values) - 1)
+
+  label_values_numeric <- suppressWarnings(as.numeric(label_values))
+
+  # which labels contain only number and hence are terminal node predictions
+  numeric_only_labels <- which(!is.na(label_values_numeric))
+
+  # which labels contain characters
+  character_labels <- which(grepl("[[:alpha:]]", label_values))
+
+  # remove any numeric only labels that have scientific notation
+  if (any(character_labels %in% numeric_only_labels)) {
+
+    character_labels <- character_labels[-which(character_labels %in% numeric_only_labels)]
+
+  }
+
+  # which labels contain numerics
+  numeric_labels <- which(grepl("[[:digit:]]", label_values))
+
+  node_predictions <- rep(NA, nrow(node_table))
+
+  node_predictions[numeric_only_labels] <- label_values_numeric[numeric_only_labels]
+
+  node_table$predictions <- node_predictions
+
+  #----------------------------------------------------------------------------#
+  # Section 4. Get split information ----
+  #----------------------------------------------------------------------------#
+
+  # extract part of the file containing links between nodes
+  edges_section <- gv_import[(edges_line + 1):(end_edges_line - 1)]
+
+  edges_section <- gsub("\"", "", edges_section)
+
+  edges_section_split <- strsplit(edges_section, " \\[label=")
+
+  edges_direction <- sapply(edges_section_split, "[", 1)
+
+  edges_label <- sapply(edges_section_split, "[", 2)
+
+  edges_label <- substr(edges_label, 1, nchar(edges_label) - 1)
+
+  edges_direction_split <- strsplit(edges_direction, " -> ")
+
+  edges_direction_from <- sapply(edges_direction_split, "[", 1)
+
+  edges_direction_to <- sapply(edges_direction_split, "[", 2)
+
+  # assumes there are only two splits per node
+  if (any(table(edges_direction_from) != 2)) {
+
+    stop("expecting two splits per node, instead got; \n",
+         paste(names(table(edges_direction_from)), collapse = " "),
+         "\n",
+         paste(table(edges_direction_from), collapse = " "))
+
+  }
+
+  node_table$left_split <- rep(NA, nrow(node_table))
+  node_table$right_split <- rep(NA, nrow(node_table))
+  node_table$left_split_levels <- rep(NA, nrow(node_table))
+  node_table$right_split_levels <- rep(NA, nrow(node_table))
+
+  # loop through each parent node and extract child nodes and levels for each split
+  for (parent_node in unique(edges_direction_from)) {
+
+    child_nodes <- edges_direction_to[which(edges_direction_from == parent_node)]
+
+    node_table$left_split[node_table$node == parent_node] <- child_nodes[1]
+    node_table$right_split[node_table$node == parent_node] <- child_nodes[2]
+
+    split_levels <- edges_label[which(edges_direction_from == parent_node)]
+
+    node_table$left_split_levels[node_table$node == parent_node] <- split_levels[1]
+    node_table$right_split_levels[node_table$node == parent_node] <- split_levels[2]
+
+  }
+
+  # replace the \\n separator in the split levels with |
+  node_table$left_split_levels <- gsub("\\\\n", "|", node_table$left_split_levels)
+  node_table$right_split_levels <- gsub("\\\\n", "|", node_table$right_split_levels)
+
+  # remove trailing | from columns
+  node_table$left_split_levels <- substr(node_table$left_split_levels,
+                                         1,
+                                         nchar(node_table$left_split_levels) - 1)
+  node_table$right_split_levels <- substr(node_table$right_split_levels,
+                                          1,
+                                          nchar(node_table$right_split_levels) - 1)
+
+  # record which direction missing values go
+  node_table$NA_direction <- rep(NA, nrow(node_table))
+  node_table$NA_direction[grepl("[NA]", node_table$left_split_levels)] <- "left"
+  node_table$NA_direction[grepl("[NA]", node_table$right_split_levels)] <- "right"
+
+  # extract the label part of the text for that node
+  node_table$node_text_label <- label_values
+
+  node_table$node_variable_type <- rep(NA, nrow(node_table))
+
+  # record categorical split variables
+  node_table$node_variable_type[character_labels[!character_labels %in% numeric_labels]] <- "categorical"
+
+  # record numerical split variables
+  node_table$node_variable_type[character_labels[character_labels %in% numeric_labels]] <- "numeric"
+
+  node_text_label_split <- strsplit(node_table$node_text_label, "<")
+  node_text_label_split_name <- sapply(node_text_label_split, "[", 1)
+  node_text_label_split_point <- sapply(node_text_label_split, "[", 2)
+
+  # get split column
+  node_table$split_column <- rep(NA, nrow(node_table))
+
+  node_table$split_column[which(node_table$node_variable_type == 'categorical')] <- node_table$node_text_label[which(node_table$node_variable_type == 'categorical')]
+
+  node_table$split_column[which(node_table$node_variable_type == 'numeric')] <- node_text_label_split_name[which(node_table$node_variable_type == 'numeric')]
+
+  node_table$split_column <- gsub(' ', '', node_table$split_column)
+
+  # get split points for numeric variables
+  node_table$node_split_point <- rep(NA, nrow(node_table))
+
+  node_table$node_split_point[which(node_table$node_variable_type == 'numeric')] <- as.numeric(node_text_label_split_point[which(node_table$node_variable_type == 'numeric')])
+
+  #----------------------------------------------------------------------------#
+  # Section 5. Return tree in tabular structure ----
+  #----------------------------------------------------------------------------#
+
+  return(node_table)
+
+}
+
+
+
+
+
+
---
+++ H2OTreeConvertR/R/h2o_tree_convertR.R
@@ -0,0 +1,181 @@
+#' Convert h2o gbm or drf to tabular structure
+#'
+#' Takes a h2o tree based model (gbm or drf) and returns a \code{list} of
+#' \code{data.frame}s representing each tree in the model in tabular structure.
+#'
+#' @param h2o_model gbm or drf h2o model.
+#' @param output_subdir directory to output intermediate files (mojo .zip, .gv
+#' and model.ini files) to. Default is location is current working dir. Files
+#' are put into a sudir with date-time in name to avoid conflicts.
+#' @param delete_intermediate_files should intermediate files output in
+#'  processing be deleted? Default = \code{TRUE}.
+#'
+#' @return returns a \code{list} containing a \code{data.frame} for each tree
+#' containing the tree structure. Tree structure \code{data.frame}s contain the
+#' following columns;
+#' \itemize{
+#'   \item{"node"} {name of the node in the tree}
+#'   \item{"node_text"} {complete text associated with the node}
+#'   \item{"predictions"} {predicted values for terminal nodes or NA}
+#'   \item{"left_split"} {left child node or NA if a terminal node}
+#'   \item{"right_split"} {right child node or NA if a terminal node}
+#'   \item{"left_split_levels"} {levels of the split variable that are sent to
+#'   the left child node from the current node, separated by |}
+#'   \item{"right_split_levels"} {levels of the split variable that are sent
+#'   to the left child node from the current node, separated by |}
+#'   \item{"NA_direction"} {the direction missing values are sent from the
+#'   current node}
+#'   \item{"node_text_label"} {the label arg of node_text, specifically the
+#'   split condition for numeric split variables or the variable name for
+#'   categorical variables}
+#'   \item{"node_variable_type"} {split variable type for node, either
+#'   categorical or numeric}
+#'   \item{"split_column"} {the name of the split column for the current node}
+#'   \item{"node_split_point"} {the split value for the current node if numeric,
+#'   otherwise NA}
+#' }
+#'
+#'
+#' @examples
+#' library(h2o)
+#'
+#' h2o.init()
+#'
+#' prostate.hex = h2o.uploadFile(path = system.file("extdata",
+#'                                                  "prostate.csv",
+#'                                                  package = "h2o"),
+#'                               destination_frame = "prostate.hex")
+#'
+#' prostate.hex["RACE"] = as.factor(prostate.hex["RACE"])
+#'
+#' prostate.hex["DPROS"] = as.factor(prostate.hex["DPROS"])
+#'
+#' expl_cols <- c("AGE", "RACE", "DPROS", "DCAPS", "PSA", "VOL", "GLEASON")
+#'
+#' prostate.gbm = h2o.gbm(x = expl_cols,
+#'                        y = "CAPSULE",
+#'                        training_frame = prostate.hex,
+#'                        ntrees = 5,
+#'                        max_depth = 5,
+#'                        learn_rate = 0.1)
+#'
+#' h2o_trees <- h2o_tree_convertR(h2o_model = prostate.gbm)
+#'
+#' @export
+h2o_tree_convertR <- function(h2o_model,
+                              output_subdir = getwd(),
+                              delete_intermediate_files = TRUE) {
+
+  #----------------------------------------------------------------------------#
+  # Function Layout: ----
+  # Section 0. Input checking
+  # Section 1. Export h2o model to mojo
+  # Section 2. Convert trees in mojo .zip to .gv files
+  # Section 3. Convert .gv files to data.frame structure
+  # Section 4. Optionally remove directory with intermediate files
+  # Section 5. Return tree structure data.frames
+  #----------------------------------------------------------------------------#
+
+  #----------------------------------------------------------------------------#
+  # Section 0. Input checking ----
+  #----------------------------------------------------------------------------#
+
+  h2o_model_classes <- c('H2ORegressionModel', 'H2OBinomialModel')
+
+  if (!class(h2o_model) %in% h2o_model_classes) {
+
+    stop('accepted classes for h2o_model are; ',
+         h2o_model_classes,
+         ', but got; ',
+         class(h2o_model))
+
+  }
+
+  h2o_model_algos <- c('gbm', 'drf')
+
+  if (!h2o_model@algorithm %in% h2o_model_algos) {
+
+    stop('accepted model types for h2o_model are; ',
+         h2o_model_algos,
+         ', but got; ',
+         h2o_model@algorithm)
+
+  }
+
+  output_dir <- paste0(output_subdir,
+                       .Platform$file.sep,
+                       Sys.Date(),
+                       "_",
+                       gsub(":", "-", format(Sys.time(), "%X")),
+                       "_H2OTreeConvertR-Intermediate")
+
+  if (dir.exists(output_dir)) {
+
+    stop("output directory already exists: ",
+         output_dir)
+
+  } else {
+
+    cat("creating output directory", output_dir, "\n")
+
+    dir.create(output_dir)
+
+  }
+
+  #----------------------------------------------------------------------------#
+  # Section 1. Export h2o model to mojo ----
+  #----------------------------------------------------------------------------#
+
+  cat("saving model mojo to", output_dir, "\n")
+
+  h2o::h2o.saveMojo(object = h2o_model,
+                    path = output_dir)
+
+  #----------------------------------------------------------------------------#
+  # Section 2. Convert trees in mojo .zip to .gv files ----
+  #----------------------------------------------------------------------------#
+
+  cat("converting trees in mojo to .gv files", "\n")
+
+  mojo_file = file.path(output_dir, paste0(h2o_model@model_id, ".zip"))
+
+  # locate h2o.jar file
+  h2o_jar_file = system.file("java", "h2o.jar", package = "h2o")
+
+  output_gv_files <- trees_to_gvs(h2o_jar = h2o_jar_file,
+                                  mojo_zip = mojo_file,
+                                  gv_output_dir = output_dir)
+
+  #----------------------------------------------------------------------------#
+  # Section 3. Convert .gv files to data.frame structure ----
+  #----------------------------------------------------------------------------#
+
+  cat("parsing .gv structures to data.frames", "\n")
+
+  tree_dfs <- lapply(output_gv_files, gv_to_table)
+
+  #----------------------------------------------------------------------------#
+  # Section 4. Optionally remove directory with intermediate files ----
+  #----------------------------------------------------------------------------#
+
+  if (delete_intermediate_files) {
+
+    cat("deleting intermediate files directory", output_dir, "\n")
+
+    unlink(output_dir, recursive = TRUE)
+
+  }
+
+  #----------------------------------------------------------------------------#
+  # Section 5. Return tree structure data.frames ----
+  #----------------------------------------------------------------------------#
+
+  return(tree_dfs)
+
+}
+
+
+
+
+
+
